f5d17d38620dc238c5ec02587de149e0a0fa5e27
cxyang1997 <cxyang@cs.utexas.edu>
2022-08-19 04:11:21 +0000

    working on CPO versions

--- sandbox/cpo/safety_constraints/cartpole.py
+++ sandbox/cpo/safety_constraints/cartpole.py
@@ -6,18 +6,21 @@ import numpy as np
 # x-position constraint. 
 class CartpoleSafetyConstraint(SafetyConstraint, Serializable):
 
-    def __init__(self, max_value=1., lim=0.01, abs_lim=False, idx=0, **kwargs):
+    def __init__(self, max_value=1., lim=0.01, abs_lim=False, idx=0, CPO_version='CPO', **kwargs):
         self.lim = lim
         self.max_value = max_value
         self.abs_lim = abs_lim
         self.idx = idx
         self.verification_length = 10
+        self.CPO_version = CPO_version # ['CPO', 'CPO_unsafety', 'CPO_safety']
         Serializable.quick_init(self, locals())
         super(CartpoleSafetyConstraint,self).__init__(max_value, **kwargs)
 
     def evaluate(self, path):
         # measure the first 10 steps \in [-0.01, 0.01]
         # return np.abs(path['observations'][:10, self.idx]) <= self.lim
+        if self.CPO_version == 'CPO':
+            
         if path['observations'].shape[0] > self.verification_length :
             safety_res = np.abs(path['observations'][:self.verification_length, self.idx]) <= self.lim
             padding = np.abs(path['observations'][self.verification_length:, self.idx]) >= 0 # all true
